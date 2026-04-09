6c16024fccabfa595c99de39b346db883c2aa95e
BOOK-NC2MNS9P6L\vitpo <vitornsc7@gmail.com>
2026-04-09 11:04:02 -0300

    feat: implementar tabs da página de turmas

---
+++ src/pages/professor/turmas/tabs/AvaliacoesTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AvaliacoesTab = () => {
+    return (
+        <p className="text-sm text-gray-400 italic py-8">
+            As avaliações ainda não estão disponíveis.
+        </p>
+    );
+};
+
+export default AvaliacoesTab;
---
+++ src/pages/professor/turmas/tabs/FaltasTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaltasTab = () => {
+    return (
+        <p className="text-sm text-gray-400 italic py-8">
+            As faltas ainda não estão disponíveis.
+        </p>
+    );
+};
+
+export default FaltasTab;
