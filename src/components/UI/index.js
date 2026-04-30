c56bac8414758647eee04c056000431620b4bb38
BOOK-NC2MNS9P6L\vitpo <vitornsc7@gmail.com>
2026-04-30 14:16:12 -0300

    feat: exporta componente NumberInput no indice de UI

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -2,6 +2,7 @@ export { default as Button } from './Button';
 export { default as ActionMenu } from './ActionMenu';
 export { default as Checkbox } from './Checkbox';
 export { default as Input } from './Input';
+export { default as NumberInput } from './NumberInput';
 export { default as Select } from './Select';
 export { default as Card } from './Card';
 export { default as Tag } from './Tag';
