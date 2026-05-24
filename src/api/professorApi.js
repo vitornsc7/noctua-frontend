179988e27381936d88cdb554023d86b869203617
BOOK-NC2MNS9P6L\vitpo <vitornsc7@gmail.com>
2026-05-24 17:37:39 -0300

    chore: increment dashboard page

--- src/api/professorApi.js
+++ src/api/professorApi.js
@@ -3,3 +3,7 @@ import client from './client';
 export const getLimites = () => client.get('/professores/limites');
 
 export const atualizarLimites = (data) => client.put('/professores/limites', data);
+
+export const getAvisosPendentes = () => client.get('/professores/avisos');
+
+export const getDashboardMetricas = () => client.get('/professores/metricas');
